61960297f971d903f36777f761926c5d3eb79825
Trung-Luong-1607 <60788442+Trung-Luong-1607@users.noreply.github.com>
2023-04-14 09:55:21 +0700

    register

--- rent-car-client/src/app/app.module.ts
+++ rent-car-client/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { NoComponent } from './view/core/no/no.component';
 import { CarouselModule } from 'primeng/carousel';
 import { RentComponent } from './view/core/rent/rent.component';
 import { RadioButtonModule } from 'primeng/radiobutton';
+import { RegisterComponent } from './view/register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,6 +38,7 @@ import { RadioButtonModule } from 'primeng/radiobutton';
     DetailComponent,
     NoComponent,
     RentComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
